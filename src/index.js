ec8045fa6170b3b36bc65e15c7d1fd3920462973
stanleyamate <60644318+stanleyamate@users.noreply.github.com>
2022-04-03 08:30:34 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-import AuthProvider from './context/AuthProvider'
+import { AuthProvider } from './context/AuthProvider'
 import Navbar from './components/navbar/Navbar';
 
 ReactDOM.render(
